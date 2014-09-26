aa6d0c9d94d4f2a469c673d710472a9173c79c87
andres-liiver <andres.liiver@gmail.com>
2014-09-26 18:50:12 +0300

    Finished Kodutoo_5. Added scarlet.txt and hound.txt

--- Kodutoo_5/Kodutoo_5_Andres.py
+++ Kodutoo_5/Kodutoo_5_Andres.py
@@ -1,67 +1,93 @@
-def process_file(file, *functions, printStr = ""):
-    '''
-    Calls variable number of functions on the file.
-    @arg file - handle to the file
-    @arg *functions - passes the file handle to the functions and calls them
-    @arg printResult - prints result of function if True
-    @returns no return value
-    '''
-    for function in functions:
-        file.seek(0)
-        if printStr != "":
-            print(function(file), printStr)
-        else:
-            function(file)
+import re
 
-def count_words(file, unique = False):
+def file_get_words(*fileNames):
     '''
-    Counts the words in a file.
+    Takes a variable number of fileNames and returns
+    a two-dimensional list of words for each file.
+    @arg *fileNames - variable number of file names.
+    @returns list of words for each file
+    '''
+    words = []
+
+    for fileName in fileNames:
+        file = open(fileName)
+        words.append(words_in_file_re(file))
+        file.close()
+
+    return words
+
+#######################################
+
+def words_in_file(file):
+    '''
+    Returns list of words in a file.
     @arg file - handle to file
-    @arg unique - count only unique words. Default False.
-    @returns no return value. Prints result.
+    @returns list of words
     '''
-    if unique:
-        words = set()
-    else:
-        words = []
-        
+    file_contents = file.read()
+    words = []
     word = ""
     
-    for line in file:
-        for char in line:
-            if char.isalpha():
-                word += char
-            else:
-                if word != "":
-                    if unique:
-                        words.add(word)
-                    else:
-                        words.append(word)
-                        
-                    word = ""
+    for char in file_contents:
+        if char.isalpha():
+            word += char
+        else:
+            if word != "":
+                words.append(word)
+                word = ""
 
     if word != "":
-        if unique:
-            words.add(word)
-        else:
-            words.append(word)
+        words.append(word)
 
-    return len(words)
+    return words
 
-def count_unique_words_in_files(file1, file2):
+#######################################
+
+def words_in_file_re(file):
     '''
-    Counts the number of unique words in two files.
-    @arg file1 - handle to file1
-    @arg file2 - handle to file2
-    @returns no return value. Prints result.
+    Returns list of words in a file using regular expression.
+    @arg file - handle to file
+    @returns list of words
     '''
-    count1 = count_words(file1, True)
-    count2 = count_words(file2, True)
-    return len(count1 | count2)
+    return re.findall(r'\w+', file.read())
 
-file1 = open("scarlet.txt")
-file2 = open("hound.txt")
-process_file(file1, count_words, printStr="number of words in " + file1.name)
-process_file(file2, count_words, printStr="number of words in " + file2.name)
+#######################################
 
-# pooleli
+def print_results(functions, printStrs):
+    '''
+    Higher-order function that calls variable number of functions and
+    prints the results with the corresponding printStr appended from printStrs.
+    @arg functions - list of functions to call
+    @arg printStr - list of strings to append to print of each result
+    @returns no return value
+    '''
+    for i, function in enumerate(functions):
+        result = function()
+        print(result, printStrs[i])
+
+#######################################
+
+file1 = "scarlet.txt"
+file2 = "hound.txt"
+words = file_get_words("scarlet.txt", "hound.txt")
+words_unique = [set(words[0]), set(words[1])]
+
+functions = [
+    lambda: len(words[0]), lambda: len(words[1]),
+    lambda: len(words_unique[0]), lambda: len(words_unique[1]),
+    lambda: len(words_unique[0] | words_unique[1]),
+    lambda: len(words_unique[0] - words_unique[1]),
+    lambda: len(words_unique[1] - words_unique[0]),
+    lambda: len(words_unique[0] & words_unique[1])
+]
+
+printStrs = [
+    "words in file " + file1, "words in file " + file2,
+    "unique words in file " + file1, "unique words in file " + file2,
+    "total unique words in files " + file1 + " and " + file2,
+    "unique words in " + file1 + " not present in " + file2,
+    "unique words in " + file2 + " not present in " + file1,
+    "unique words that are in both " + file1 + " and " + file2
+]
+
+print_results(functions, printStrs)
