28cbf62f0cb81842029e6b1125c4a669e256d333
Andres <andres-liiver@users.noreply.github.com>
2014-10-14 13:19:37 +0300

    Update Kodutoo_8_Andres.py

--- Kodutoo_8/Kodutoo_8_Andres.py
+++ Kodutoo_8/Kodutoo_8_Andres.py
@@ -4,3 +4,20 @@ Kodutoo 8
 Andres Liiver
 '''
 
+import unittest
+from Roman_numerals import convert
+import re
+
+class Test(unittest.TestCase):
+    def test_1(self):
+        file = open("roman_numerals.txt")
+
+        for line in file:
+            test = re.findall(r'[^,;\s]+', line)
+            print(test)
+            self.assertEqual(convert(test[0]), int(test[1]))
+
+        file.close()
+        
+if __name__ == "__main__":
+    unittest.main(verbosity=5, exit=False)
