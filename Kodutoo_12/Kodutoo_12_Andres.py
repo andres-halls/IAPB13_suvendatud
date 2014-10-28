58263e5c6a0f6f8fb811f62569d1b5c5bfad9a9c
Andres <andres-liiver@users.noreply.github.com>
2014-10-28 13:22:39 +0200

    Added Kodutoo_12

---
+++ Kodutoo_12/Kodutoo_12_Andres.py
@@ -0,0 +1,17 @@
+'''
+Kodutoo 12
+28.10.2014
+Andres Liiver
+'''
+
+def pair_frequency(words):
+    pass
+
+def create_heat_map(file):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
