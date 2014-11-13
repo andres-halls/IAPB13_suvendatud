a06809629a47be541b702ed5f282fe8ed3d70458
andres-liiver <andres.liiver@gmail.com>
2014-11-13 19:57:58 +0200

    Finished Kodutoo_15

--- Kodutoo_15/Kodutoo_15_Andres.py
+++ Kodutoo_15/Kodutoo_15_Andres.py
@@ -4,22 +4,44 @@ Kodutoo 15
 Andres Liiver
 '''
 
-import re
-import sympy as sp
-import numpy as np
 from matplotlib import pyplot as plt
 
 def get_data_from_csv(file):
     data = {}
-    file_contents = file.read()
-    data['years'] = re.findall(r'(?!")\d{4}(?=")', file_contents)
+    data['years'] = []
+    data['births'] = []
+    data['deaths'] = []
+    data['iive'] = []
+
+    for row in file:
+        splitRow = row.split(';')
+        data['years'].append( int(splitRow[0][1:-1]) )
+        data['births'].append( int(splitRow[1]) )
+        data['deaths'].append( int(splitRow[2]) )
+        data['iive'].append( int(splitRow[3]) )
+
     return data
 
 def main():
-    file = open("RV030s.csv")
-    data = get_data_from_csv(file)
-    print(data)
-    file.close()
+    try:
+        # Get data from CSV
+        file = open("RV030s.csv")
+        data = get_data_from_csv(file)
+
+        # Plot the data
+        fig, ax1 = plt.subplots()
+        ax1.set_xlabel("Aasta")
+        ax1.set_ylabel("Inimeste arv")
+
+        ax1.plot(data['years'], data['births'], "g")
+        ax1.plot(data['years'], data['deaths'], "r")
+        ax2 = ax1.twinx()
+        ax2.plot(data['years'], data['iive'], "b")
+        ax2.set_ylabel("Iive")
+        plt.show()
+
+    finally:
+        file.close()
 
 if __name__ == "__main__":
     main()
