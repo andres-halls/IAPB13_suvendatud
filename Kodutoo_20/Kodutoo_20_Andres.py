8647c4a4ca35cbfa26f5523f11b3d631b44669cd
andres-liiver <andres.liiver@gmail.com>
2014-12-07 22:29:20 +0200

    Finished Kodutoo_20

--- Kodutoo_20/Kodutoo_20_Andres.py
+++ Kodutoo_20/Kodutoo_20_Andres.py
@@ -6,14 +6,39 @@ Andres Liiver
 
 from tree import *
 
+def read_words(file):
+    file_contents = file.read()
+    word = ""
+
+    for char in file_contents:
+        char = char.lower()
+        if char.isalpha():
+            word += char
+        else:
+            if word != "":
+                yield word
+                word = ""
+
 def main():
-    left = Tree("blah")
-    right = Tree("zeus")
-    tree = Tree("car", left=left, right=right)
-    tree.add(Tree("test"))
-    tree.add(Tree("test"))
-    tree.maxDepth()
-    tree = tree
+    try:
+        file = open("hound.txt", encoding="utf8")
+    except Exception as e:
+        print("Error occured when opening file:", e)
+    else:
+        words = read_words(file)
+        tree = Tree(next(words))
+        [tree.add(Tree(word)) for word in words]
+        node, dist = tree.search("was")
+        if node:
+            node.printTree(1)
+            print('Distance from root:', dist)
+
+        print(tree.maxDepth())
+        #outputFile = open("result.txt", "w", encoding="utf8")
+        #tree.printToFile(outputFile)
+        #outputFile.close()
+    finally:
+        file.close()
 
 if __name__ == "__main__":
     main()
--- Kodutoo_20/tree.py
+++ Kodutoo_20/tree.py
@@ -6,35 +6,71 @@ class Tree:
         self.right = right
 
     def add(self, node):
-        if self.data == node.data:
+        if node.data == self.data:
             self.count += 1
             return
 
-        if self.data > node.data: # insert left
+        if node.data < self.data:
             if self.left is None:
                 self.left = node
             else:
                 self.left.add(node)
-        else: # insert right
+        else:
             if self.right is None:
                 self.right = node
             else:
                 self.right.add(node)
 
-    def delete(self, node):
-        pass
+    def maxDepth(self):
+        if self.left is None and self.right is None:
+            return 0
 
-    def maxDepth(self, depth=0):
-        if self is None: return depth
+        if self.left is None:
+            lDepth = 0
+        else:
+            lDepth = self.left.maxDepth()
+
+        if self.right is None:
+            rDepth = 0
+        else:
+            rDepth = self.right.maxDepth()
+
+        return max(lDepth, rDepth) + 1
 
     def minValue(self):
-        pass
+        if self.left is None: return self
+        return self.left.minValue()
 
-    def printTree(self):
-        pass
+    def printTree(self, depth = None):
+        if depth is not None:
+            if depth == 0: return
+            depth -= 1
 
-    def search(self, word):
-        pass
+        if self.left is not None:
+            self.left.printTree(depth)
+        print(self.data, ":", self.count)
+        if self.right is not None:
+            self.right.printTree(depth)
+
+    def printToFile(self, file):
+        if self.left is not None:
+            self.left.printToFile(file)
+        file.write(self.data + " : " + str(self.count) + "\n")
+        if self.right is not None:
+            self.right.printToFile(file)
+
+    def search(self, word, _dist = 0):
+        if word == self.data:
+            return (self, _dist) # found
+        else:
+            if self.left is None and self.right is None:
+                return (False, None) # not found
+            if word < self.data:
+                _dist += 1
+                return self.left.search(word, _dist)
+            else:
+                _dist += 1
+                return self.right.search(word, _dist)
 
 if __name__ == "__main__":
     pass
